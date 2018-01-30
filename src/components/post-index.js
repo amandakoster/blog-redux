35c43c982b400e109fe4e97e2f90d3a1c0009e1a
Amanda Koster <amandakoster@gmail.com>
2018-01-29 16:02:03 -0800

    scaff

---
+++ src/components/post-index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PostIndex extends React.Component{
+    render() {
+        return (
+            <div>
+            Post Index
+            </div>
+
+        );
+    }
+}
+
+export default PostIndex
+
